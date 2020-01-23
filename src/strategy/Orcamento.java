32543401d9fef3d0aeaecf333294df739ccd37b0
Renan Azzolim <azzolimrenan2@gmail.com>
2020-01-22 23:24:07 -0200

    strategy

---
+++ src/strategy/Orcamento.java
@@ -0,0 +1,15 @@
+package strategy;
+
+public class Orcamento {
+
+	private double valor;
+
+	public Orcamento(Double valor) {
+		this.valor = valor;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+}
