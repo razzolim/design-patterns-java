54d5d14f14017727a3ce15ea9cfbac2e3d0dcaca
Renan Azzolim <azzolimrenan2@gmail.com>
2020-07-10 21:46:06 -0300

    adjustments

--- chain-of-responsibility/src/chainofresponsibility/CalculadorDeDesconto.java
+++ chain-of-responsibility/src/chainofresponsibility/CalculadorDeDesconto.java
@@ -1,5 +1,18 @@
 package chainofresponsibility;
 
+/**
+ * O padrão Chain of Responsibility cai como uma luva quando temos uma lista de
+ * comandos a serem executados de acordo com algum cenário em específico, e
+ * sabemos também qual o próximo cenário que deve ser validado, caso o anterior
+ * não satisfaça a condição.
+ * <br><br>
+ * Nesses casos, o Chain of Responsibility nos possibilita a separação de
+ * responsabilidades em classes pequenas e enxutas, e ainda provê uma maneira
+ * flexível e desacoplada de juntar esses comportamentos novamente.
+ * 
+ * @author Renan
+ *
+ */
 public class CalculadorDeDesconto {
 
 	public double calcula(Orcamento orcamento) {
--- strategy/src/strategy/CalculadorDeImpostos.java
+++ strategy/src/strategy/CalculadorDeImpostos.java
@@ -1,7 +1,24 @@
 package strategy;
 
+/**
+ * O padrão Strategy é muito útil quando temos um conjunto de algoritmos
+ * similares, e precisamos alternar entre eles em diferentes pedaços da
+ * aplicação. No exemplo do vídeo, temos diferentes maneiras de calcular o
+ * imposto, e precisamos alternar entre elas.
+ * <br>
+ * O Strategy nos oferece uma maneira flexível para escrever diversos algoritmos
+ * diferentes, e de passar esses algoritmos para classes clientes que precisam
+ * deles. Esses clientes desconhecem qual é o algoritmo "real" que está sendo
+ * executado, e apenas mandam o algoritmo rodar. Isso faz com que o código da
+ * classe cliente fique bastante desacoplado das implementações concretas de
+ * algoritmos, possibilitando assim com que esse cliente consiga trabalhar com N
+ * diferentes algoritmos sem precisar alterar o seu código.
+ * 
+ * @author Renan
+ *
+ */
 public class CalculadorDeImpostos {
-	
+
 	public void realizaCalculo(Orcamento orcamento, Imposto impostoQualquer) {
 		double valor = impostoQualquer.calcula(orcamento);
 		System.out.println(valor);
